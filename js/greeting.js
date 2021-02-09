4269bef4675dac64fdd2badd93f0ef744e804446
AitorAmbite <51913218+AitorAmbite@users.noreply.github.com>
2021-02-09 20:13:08 +0100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,20 +3,18 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' Aguacate furioso';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
-const gree3 = 'Good afternoon  ';
-const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
+const gree1 = 'Tira a dormir coño!  ';
+const gree2 = 'Buenos dias!  ';
+const gree3 = 'BUenas tardes  ';
+const gree4 = 'Buenas noches,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
   document.getElementById('greetings').innerText = gree1 + name;
-} else if (hour >= 6 && hour < 12) {
+}else if (hour >= 6 && hour < 12) {
   document.getElementById('greetings').innerText = gree2 + name;
 } else if (hour >= 12 && hour < 17) {
   document.getElementById('greetings').innerText = gree3 + name;
